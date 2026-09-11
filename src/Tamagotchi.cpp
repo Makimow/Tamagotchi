d67914ffb490daa047d9b8989176be66cfe9b7b6
makimo <hornoyantonin@gmail.com>
2026-09-11 13:02:06 +0200

    Initial commit : structure finale du projet

---
+++ src/Tamagotchi.cpp
@@ -0,0 +1,10 @@
+/**
+ * @brief Représente l'animal virtuel et ses statistiques.
+ */
+class Tamagotchi {
+    /**
+     * @brief Diminue la faim de 10 points.
+     * @param quantite la quantite de nourriture donnee
+     */
+    void nourrir(int quantite);
+};
